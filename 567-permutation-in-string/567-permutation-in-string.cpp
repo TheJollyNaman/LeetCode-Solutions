222581481fb6e46d80f85b264c73306b493ce0a0
Naman Jolly <naman.jolly2003@gmail.com>
2022-02-03 22:13:59 +0530

    Time: 4 ms (95.71%), Space: 7.4 MB (40.02%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        if(s1.size() > s2.size()) 
+            return false;
+        
+        vector<int>count1(26,0), count2(26,0);
+        
+        for(int i = 0; i < s1.size(); i++) {
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
+        }
+        
+        if(count1 == count2) 
+            return true;
+        
+        int start = 0, end = s1.size();
+        while(end < s2.size()){
+            count2[s2[start] - 'a']--;
+            count2[s2[end] - 'a']++;
+          
+            if(count1 == count2) 
+                return true;
+            
+            end++; 
+            start++;
+        }
+        
+        return false;
+    }
+};
