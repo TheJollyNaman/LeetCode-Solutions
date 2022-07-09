c25905907fb2c817d51e227b7a50c9d9a65d5fd9
Naman Jolly <naman.jolly2003@gmail.com>
2022-07-09 12:12:16 +0530

    Time: 295 ms (55.48%), Space: 93.9 MB (33.65%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        vector<int> dp(nums.size(),0);
+        priority_queue<pair<int,int>> pq;
+        
+        dp[0] = nums[0];
+        pq.push({nums[0],0});
+        
+        for(int i=1;i<nums.size();i++){
+            while(pq.top().second+k<i){
+                pq.pop();
+            }
+            dp[i]=nums[i]+pq.top().first;
+            pq.push({dp[i],i});
+        }
+        return dp[nums.size()-1];
+    }
+};
