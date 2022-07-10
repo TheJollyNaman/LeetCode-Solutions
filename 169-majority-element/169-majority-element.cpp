057fa115b9ec03912ce6019f108408eeea3cce35
Naman Jolly <naman.jolly2003@gmail.com>
2022-07-10 15:10:50 +0530

    Time: 22 ms (82.39%), Space: 19.5 MB (91.96%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0,maj = 0;
+        for(int i=0;i<nums.size();i++){
+            if(count == 0){
+                maj=nums[i];
+            }
+            if(nums[i]==maj){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return maj;
+    }
+};
+
